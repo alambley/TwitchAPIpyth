f03fb810121e4e393c7cb4db10ea4f10cbafca6c
alambley <lambleyalexander@gmail.com>
2017-12-12 00:35:08 +0000

    Updated to work with Twitch's new API

--- TwitchAPIpyth.py
+++ TwitchAPIpyth.py
@@ -1,34 +1,54 @@
+#!/usr/bin/python3
+
 import sys
 import urllib.request
 import json
-#import request                                                    #I really liked request, but I generally prefer solutions that don't require non-standard libraries if possible.
+import os
 
-if len(sys.argv) != 2:
+def main():
+
+  os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+  clientID = ""
+
+  if os.path.isfile("clientkey.txt"):
+    clientID = fileToString("clientkey.txt").replace("\n","").strip()
+  else:
+    print("Twitch API now requires you to register your app to access the API")
+    print("https://dev.twitch.tv/docs/api")
+    print("Add your Client-ID to a text file called 'clientkey.txt' in the same dir as this script")
+
+  if len(sys.argv) != 2:
     print("Program is envoked with the streamers name as a command line argument.")
-    print(len(sys.argv))
     sys.exit()
-streamer = sys.argv[1]
-viewerendpoint = "https://api.twitch.tv/kraken/streams/" + streamer
+  streamer = sys.argv[1]
+  viewerendpoint = "https://api.twitch.tv/kraken/streams/" + streamer
 
-try:
-    resp = urllib.request.urlopen(viewerendpoint).read()           #resp = requests.get(viewerendpoint)
-except urllib.error.URLError:                                      #except request.exceptions.ConnectionError:
-    print("API access failed.")
-    print("Make sure the Twitch account actually exists.")
-    print("If you're sure it is, check your Internet connection and the API itself.")   
+  try:
+    request = urllib.request.Request(viewerendpoint,data=None,headers={"Client-ID" : clientID})
+    resp = urllib.request.urlopen(request).read()
+  except urllib.error.URLError as e:
+    print("API access failed ({0})".format(e))
+
     sys.exit();
-  
-json = json.loads(resp.decode())                                   #json = resp.json()
-    
-try:
-    numbviewers = json["stream"]["viewers"]
-    streamer = json["stream"]["channel"]["display_name"]
-    gameplaying = json["stream"]["channel"]["game"]
-    status = json["stream"]["channel"]["status"]
+
+  jsonResp = json.loads(resp.decode())
+
+  try:
+    numbviewers = jsonResp["stream"]["viewers"]
+    streamer = jsonResp["stream"]["channel"]["display_name"]
+    gameplaying = jsonResp["stream"]["channel"]["game"]
+    status = jsonResp["stream"]["channel"]["status"]
     print(streamer + " is streaming " + gameplaying + " with " + str(numbviewers) + " viewers.")
     print(status)
-except TypeError:
-    print(streamer + " is not streaming right now.")
-    
+  except TypeError:
+    print(streamer + " is not streaming right now or doesn't exist.")
+
+def fileToString(path):
+  with open(path,"r") as f:
+    return f.read()
+
+if __name__ == "__main__":
+  main()
 
 
